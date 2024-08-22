3a313b22257870455b9b4d070989433b8e431b55
Sowmya <chattisowmya@gmail.com>
2024-08-22 16:23:45 -0400

    added DBT Task to DAG successfully

--- dags/retail.py
+++ dags/retail.py
@@ -6,6 +6,10 @@ from astro import sql as aql
 from astro.files import File
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.config import RenderConfig
+from cosmos.constants import LoadMode
 
 @dag(
     start_date=datetime(2024, 1, 1),
@@ -51,5 +55,19 @@ def retail():
         
         
         return check(scan_name, checks_subpath)
+    
     check_load()
+    
+    transform = DbtTaskGroup(
+        group_id = 'transform',
+        project_config = DBT_PROJECT_CONFIG,
+        profile_config = DBT_CONFIG,
+        render_config = RenderConfig(
+            load_method = LoadMode.DBT_LS,
+            select = ['path:models/transform']
+        )
+        
+    )
+    
+    
 retail()
