a86a7ff964b32c3d6e3700a6401c5d27547cdbba
Sowmya <chattisowmya@gmail.com>
2024-08-22 16:48:12 -0400

    Added data quality checks for facts and dimensions

--- dags/retail.py
+++ dags/retail.py
@@ -69,5 +69,13 @@ def retail():
         
     )
     
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_transform(scan_name='check_transform', checks_subpath='transform'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
+    check_transform()
+    
     
 retail()
